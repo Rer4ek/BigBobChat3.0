764ddc502d46e572ab30eb8ef4d16e69a79695b5
Nikita <118511957+Rer4ek@users.noreply.github.com>
2024-03-14 20:45:15 +0300

    0.01
    base

---
+++ BigBobicChat/ChatHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace BigBobicChat
+{
+    public class ChatHub : Hub
+    {
+        public async Task Send(string username, string message)
+        {
+            await this.Clients.All.SendAsync("Receive", username, message);
+        }
+    }
+}
---
+++ Client/MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.AspNetCore.SignalR.Client;
+using System.Net.Http;
+
+namespace Client
+{
+
+    public partial class MainWindow : Window
+    {
+        HubConnection connection;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            var handler = new HttpClientHandler();
+            handler.ServerCertificateCustomValidationCallback = HttpClientHandler.DangerousAcceptAnyServerCertificateValidator;
+
+            connection = new HubConnectionBuilder()
+                .WithUrl("https://192.168.1.113:7268/chat", options =>
+                {
+                    options.HttpMessageHandlerFactory = _ => handler;
+                })
+                .Build();
+
+            connection.On<string, string>("Receive", (user, message) =>
+            {
+                Dispatcher.Invoke(() =>
+                {
+                    var newMessage = $"{user}: {message}";
+                    MessageListBox.Items.Insert(0, newMessage);
+                });
+            });
+        }
+
+        private async void TextBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                await connection.InvokeAsync("Send", NameField.Text, Message.Text);
+            }
+        }
+
+        private async void Connect_Click(object sender, RoutedEventArgs e)
+        {
+            await connection.StartAsync();
+            Connect.IsEnabled = false;
+            await connection.InvokeAsync("Send", NameField.Text, " - Подключился");
+        }
+    }
+}
